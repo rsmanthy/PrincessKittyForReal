6864a8018ac15ffae219eda4ad8279aaf09e7f87
Ian McKibben <iamc7054@colorado.edu>
2023-03-04 14:13:30 -0700

    Created Gamemanager and classes for gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Planet planet;
+    public List<Asteriod> asteriods;
+    public List<Ship> ships;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initalize all game start stuff
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call behaviors on everything? Idk   
+    }
+}
