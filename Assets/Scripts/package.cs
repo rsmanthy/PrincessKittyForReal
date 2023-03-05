36f073a5d599ef057233eb66ec08a0e0d641626f
AJ Juenemann <alju9373@colorado.edu>
2023-03-04 22:31:30 -0700

    Belt prototype

---
+++ Assets/Scripts/package.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class package : MonoBehaviour
+{ 
+    private GameObject target;
+    private Vector2 targetLocation;
+
+    void Awake()
+    {
+        target = GameObject.Find("ConvayerBelt-Sheet_0");
+        transform.position = new Vector2(target.transform.position.x + 1.9f, target.transform.position.y + 0.08f);
+        targetLocation = new Vector2(-transform.position.x, transform.position.y);
+        
+    }
+    void Update()
+    {
+        var step =  4 * Time.deltaTime; // calculate distance to move
+        transform.position = Vector2.MoveTowards(transform.position, targetLocation, step);
+        if (Vector2.Distance(transform.position, targetLocation) < 0.001f)
+        {
+            /*
+                Increment balance based off ship ore capacity
+            */
+            Destroy(gameObject);
+        }
+    }
+}
