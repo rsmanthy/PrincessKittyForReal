4838501ac8310f2fa96f7fae02f8f21981e3c049
Roxanne Manthy <rsmanthy@gmail.com>
2023-03-04 23:49:47 -0700

    wtf is this ui

--- Assets/Scripts/IncomeCostScript.cs
+++ Assets/Scripts/IncomeCostScript.cs
@@ -1,8 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class IncomeCostScript : MonoBehaviour
-{
-   
-}
+// public class IncomeCostScript : MonoBehaviour
+// {
+// 	public int totalCost;
+
+// 	void levelCost()
+// 	{
+// 		totalCost = PlayerPrefs.GetInt("Cost", 0);
+
+// 		totalCost++;
+
+// 		PlayerPrefs.SetInt("Score", totalCost);
+// 		PlayerPrefs.Save();
+
+// 		Debug.Log("Score" + totalCost.ToString());
+// 	}
+// }
